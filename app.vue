094d4007c9eb53f0d59d7d460562f4bb1b01a7ff
maivietphong <maivietphong1796gmail.com@maivietphongs-MacBook-Pro.local>
2023-08-18 22:35:16 +0700

    create generate new array function

---
+++ app.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="header-container">
+    <v-btn @click="generateNewArray">Generate New Array</v-btn>
+    <v-btn>Bubble Sort</v-btn>
+    <v-btn>Sort</v-btn>
+  </div>
+  <div class="body-container">
+    <div v-for="row in rows" :key="row.height" class="row mx-2" :style="{ height: row.height * 3 + 'px'}"></div>
+  </div>
+</template>
+<script lang="ts" setup >
+enum STATUS_ROW  {
+  PROCESS = 'PROCESS',
+  DONE = 'DONE',
+  NONE = 'NONE'
+}
+
+interface IRow {
+  status: STATUS_ROW
+  height: number
+}
+const rows = ref<IRow[]>([])
+const rowsLength  = ref<number>(10)
+const generateNewArray = () => {
+  const newRows:IRow[] = []
+  for (let i = 0; i < rowsLength.value; i++) {
+      const rndInt = Math.floor(Math.random() * 100) + 10
+      const newRow: IRow = {
+        status: STATUS_ROW.NONE,
+        height: rndInt
+      }
+      newRows.push(newRow)
+  }
+  rows.value = [...newRows]
+}
+
+
+</script>
+<style lang="scss" scoped>
+.body-container{
+  display: flex;
+  justify-content: center;
+}
+
+.header-container{
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: cornflowerblue;
+  height: 80px;
+}
+.row{
+  width: 30px;
+  background-color: blue;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css", 'vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
